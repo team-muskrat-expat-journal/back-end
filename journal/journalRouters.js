137130a7557ac49455bdf0777f8b8eff6364974b
unknown <tompsherman@gmail.com>
2020-11-16 08:48:02 -0500

    some dummy data to play with

--- journal/journalRouters.js
+++ journal/journalRouters.js
@@ -2,6 +2,30 @@ const router = require('express').Router()
 
 const Journal = require('./journalModels')
 
+let jEntry = [
+    {
+        title: "muskrat stew festival",
+        tagline: "best place to eat muskrat in America",
+        location: "Cambridge, MD",
+        rating: 5,
+        notes: "occurs once a year; enter the leg eating championship."
+    },
+    {
+        title: "Gillett Coon Supper",
+        tagline: "best place to eat raccoon in America",
+        location: "Jonesboro, AR",
+        rating: 5,
+        notes: "be prepared to talk local Arkansas politics"
+    },
+    {
+        title: "the Roadkill Cookoff",
+        tagline: "there was no roadkill to eat here",
+        location: "Marlinton, WV",
+        rating: 2,
+        notes: "no roadkill, i think i ate a hs science class' rabbit"
+    }
+]
+
 const currentTime = new Date().toDateString()
 
 // @desc		Test end is working
@@ -9,5 +33,9 @@ const currentTime = new Date().toDateString()
 router.get('/test', (req, res) => {
     res.status(202).json({message: 'the journal router is running at ' + currentTime})
 })
+
+router.get('/', (req,res)=>{ 
+    res.status(200).json(jEntry)
+ })
 
 module.exports = router
